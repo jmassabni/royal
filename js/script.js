95391206d5b0c0728d85fca27626ea1bef2f5b3a
Paul Manoian <Paul Manoian>
2016-06-07 11:11:01 -0400

    testing gitbash

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showmenu(){
+	var menubutton = document.getElementById("mobile-menu")
+	
+	if(menubutton.style.display == "none")
+	menubutton.style.display = 'block';
+	else
+	menubutton.style.display = 'none';  
+}
